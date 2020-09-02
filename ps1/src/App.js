6d4f4a70d762d2b6c1e198730f6c1d3afeaf8311
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-09-02 13:07:38 -0700

    Plural sight practice with hooks, props, handleClick function

--- ps1/src/App.js
+++ ps1/src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment);
+	return (
+  	<button onClick={handleClick}  className="margin">
+      +{props.increment}
+    </button>
+  );
+}
+
+function Display(props) {
+	return (
+  	<div className="messageDisplay">{props.message}</div>
+  );
+}
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4 buttonSection">
+          <div>
+<Button onClickFunction={incrementCounter} increment={1}/>
+<Button onClickFunction={incrementCounter} increment={5} />
+<Button onClickFunction={incrementCounter} increment={10} />
+<Button onClickFunction={incrementCounter} increment={100} />
+<Display message={counter}/>
+</div> 
     </div>
+        </div>
+        <div className="col-md-4"></div>
+      </div>
+
+      
+
+ 
   );
 }
 
